42bde7317537c504081b2b7c96d293616e156d63
Mohamed Sofy <mediumsofy@gmail.com>
2024-10-02 04:40:02 +0300

    Add handleAddTodos function to ToDoInput Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { useState } from "react"
-import ToDoInput from "./components/ToDoInput"
-import ToDoList from "./components/ToDoList"
+import { useState } from "react";
+import ToDoInput from "./components/ToDoInput";
+import ToDoList from "./components/ToDoList";
 
 function App() {
-
-  const [todos, setTodos] =useState( [
-    'Go to the gym',
-    'Eat more fruits and vegies',
-    'Pickup the kids from school'
-  ])
-
+  const [todos, setTodos] = useState([]);
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo];
+    setTodos(newTodoList);
+  }
 
   return (
-      <>
-        <ToDoInput/>
-        <ToDoList todos={todos}/>
-      </>
-  )
+    <>
+      <ToDoInput handleAddTodos={handleAddTodos} />
+      <ToDoList todos={todos} />
+    </>
+  );
 }
 
-export default App
+export default App;
